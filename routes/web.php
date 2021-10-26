e1f5f7f54363665f22f4855100d3688ccdcd3499
Javier Ignacio Ruiz Anriquez <jiruiz2@uc.cl>
2021-10-26 00:37:26 -0300

    Revert "Revert "Feature/products""

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 
 
 /*
@@ -18,3 +19,7 @@ Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::post('login', [AuthController::class, 'createSession'])->name('createSession');
 Route::get('register', [UserController::class, 'register'])->name('register');
 Route::post('register', [UserController::class, 'create'])->name('createUser');
+Route::get('product', [ProductController::class, 'new'])->name('newProduct');
+Route::post('product', [ProductController::class, 'create'])->name('createProduct');
+Route::get('products', [ProductController::class, 'buyableProducts'])->name('products');
+Route::get('myProducts', [ProductController::class, 'myProducts'])->name('myProducts');
