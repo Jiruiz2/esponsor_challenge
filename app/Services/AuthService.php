128cebc755aa7a749f53bcdca5642ae997743ea9
jiruiz2 <jiruiz2@uc.cl>
2021-10-26 00:23:09 -0300

    Added username login

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -6,8 +6,22 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthService
 {
-    public function validLogin($credentials)
+    public function validLogin($login_data)
     {
+        $credentials = [
+            'email' => $login_data['email_or_username'],
+            'password' => $login_data['password'],
+        ];
+
+        if (Auth::attempt($credentials)) {
+            return true;
+        }
+
+        $credentials = [
+            'username' => $login_data['email_or_username'],
+            'password' => $login_data['password'],
+        ];
+
         if (Auth::attempt($credentials)) {
             return true;
         }
